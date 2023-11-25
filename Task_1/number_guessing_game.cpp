adba2e6f55ce60852ea2a56c87a59978104e503f
Upendra Raj Joshi <upendraofficial10@gmail.com>
2023-11-25 21:08:01 +0545

    Adding input validation

--- Task_1/number_guessing_game.cpp
+++ Task_1/number_guessing_game.cpp
@@ -17,20 +17,25 @@ int main()
     {
         cout << "Enter a guess between 1 and 100 : ";
         cin >> guess;
+        if(guess < 1 || guess > 100)
+        {
+            cout << "Invalid guess! Please enter a guess between 1 and 100.\n\n";    // if guess is out of range
+            continue;
+        }
         tries++;
         if (guess > number)
         {
-            cout << "Too high!\n\n";
+            cout << "Too high!\n\n";    // if guess is too high
         }
         else if (guess < number)
         {
-            cout << "Too low!\n\n";
+            cout << "Too low!\n\n";     // if guess is too low
         }
         else
         {
-            cout << "\nCorrect! You got it in " << tries << " guesses!\n";
+            cout << "\nCorrect! You got it in " << tries << " guesses!\n";     // if guess is correct
         }
-    } while (guess != number);
-    
+    } while (guess != number);    // until guess is correct
+
     return 0;
 }
